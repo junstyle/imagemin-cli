e3d0d84666d29b135b9c18a6cd68d7bec1ade716
junstyle <junstyle@git>
2018-06-01 20:21:01 +0800

    commit

--- imagemin-mozjpeg/mozjpeg/lib/index.js
+++ imagemin-mozjpeg/mozjpeg/lib/index.js
@@ -1,7 +1,7 @@
 'use strict';
 const path = require('path');
 const BinWrapper = require('bin-wrapper');
-const pkg = require('./../package.json');
+const pkg = require('../package.json');
 
 const url = `https://raw.githubusercontent.com/imagemin/mozjpeg-bin/v${pkg.version}/vendor/`;
 
